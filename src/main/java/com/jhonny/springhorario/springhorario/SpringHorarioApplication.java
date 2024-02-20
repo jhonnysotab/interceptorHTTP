be88fb00c2e1fa7e591d460fa5c6121b715ec025
jhonnysotab <jhonnysota67@gmail.com>
2024-02-19 22:58:18 -0500

    Interceptor HTTP horario de acceso al cliente

---
+++ src/main/java/com/jhonny/springhorario/springhorario/SpringHorarioApplication.java
@@ -0,0 +1,13 @@
+package com.jhonny.springhorario.springhorario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHorarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHorarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhonny/springhorario/springhorario/SpringHorarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonny.springhorario.springhorario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHorarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
